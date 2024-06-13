81c646a85926c60640007b7a3b2d9e9e26be633e
Benhur Zambiasi <benhur_zambiasi@hotmail.com>
2024-06-13 10:41:10 -0300

    accordion

---
+++ src/pages/DadosBeneficiario/Accordion/index.jsx
@@ -0,0 +1,31 @@
+/* eslint-disable react/prop-types */
+import "./accordion.scss";
+
+const Accordion = ({ children, onEdit }) => {
+  return (
+    <div className="d-flex flex-column gap-3">
+      <details>
+        <summary>
+          <span>title</span>
+          <div className="d-flex align-items-center gap-6">
+            {onEdit && (
+              <button
+                className="d-flex align-items-center gap-2 btn-edit"
+                onClick={onEdit}>
+                <span className="material-symbols-outlined arrow-ac">edit</span>
+                Alterar dados
+              </button>
+            )}
+
+            <span className="material-symbols-outlined arrow-ac">
+              expand_more
+            </span>
+          </div>
+        </summary>
+        <div>{children}</div>
+      </details>
+    </div>
+  );
+};
+
+export default Accordion;
--- src/pages/DadosBeneficiario/info/index.jsx
+++ src/pages/DadosBeneficiario/info/index.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import Accordion from "../Accordion";
 
 export const Info = () => {
   return (
     <div>
-      <h1>Informações cadastrais</h1>
+      <div className="container d-flex flex-column gap-3">
+        <Accordion>
+          <div>eira</div>
+          <div>eira</div>
+          <div>eira</div>
+        </Accordion>
+        <Accordion />
+        <Accordion />
+      </div>
+
+      <div>drawer</div>
     </div>
   );
 };
